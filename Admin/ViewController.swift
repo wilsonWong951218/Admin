55b16faae793ce3fdc16f58919cd5b1e8ccac753
wilson <wong_wei_wen@hotmail.com>
2018-05-09 20:20:38 +0800

    first

---
+++ Admin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Admin
+//
+//  Created by Mac mini on 2018/5/6.
+//  Copyright © 2018年 Mac mini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
