321b5617772ed53f66fd6addad63299a01f48838
wilson <wong_wei_wen@hotmail.com>
2018-05-16 21:26:03 +0800

    wilson1

--- Admin/ViewControllerProflie.swift
+++ Admin/ViewControllerProflie.swift
@@ -8,7 +8,72 @@
 
 import UIKit
 import Firebase
-class ViewControllerProflie: UIViewController {
+class ViewControllerProflie: UIViewController,UITableViewDelegate,UITableViewDataSource{
+   
+    @IBOutlet weak var tableView: UITableView!
+    var cellItem = ["Order1","Order2","Order3","Order4","Order5"]
+    var cellItem1 = ["Order6","Order7","Order8","Order19"]
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let button = UIButton(type: .system)
+        button.setTitle("Close", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = UIColor.orange
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(heandleClose), for: .touchUpInside)
+        return button
+        
+//        let label = UILabel()
+//        label.text = "Order Form"
+//        label.font = UIFont(name: "Chalkduster", size: 40)
+//        label.backgroundColor = UIColor.lightGray
+//        return label
+    }
+    
+    @objc func heandleClose(){
+         var indexPaths = [IndexPath]()
+        for row in cellItem.indices{
+            let indexPath = IndexPath(row: row, section: 0)
+            indexPaths.append(indexPath)
+        }
+        cellItem.removeAll()
+        tableView.deleteRows(at: indexPaths, with: .fade)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellItem.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = cellItem[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+            return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("select section:\(indexPath.section)")
+        print("select section:\(indexPath.row)")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            cellItem.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
     
    
     @IBOutlet weak var profileName: UILabel!
@@ -23,9 +88,15 @@ class ViewControllerProflie: UIViewController {
         profilePic.layer.masksToBounds = true
         
         profileName.text = profileID[2] as? String
+       tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(refreshList), for: .valueChanged)
 
     }
-    
+    @objc func refreshList(){
+        self.cellItem = cellItem1
+        tableView.reloadData()
+        tableView.refreshControl?.endRefreshing()
+    }
     
     @IBAction func logOutButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
@@ -38,7 +109,8 @@ class ViewControllerProflie: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+ 
+    
     /*
     // MARK: - Navigation
 
