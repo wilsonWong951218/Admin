263c2cf6e27551abcfb26fdbf861246fa64bafbc
Tsejunzhen <victorjunzhen@gmail.com>
2018-05-21 18:02:21 +0800

    Admin login profile 處理中

--- Admin/ViewController.swift
+++ Admin/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton.addTarget(self, action: #selector(getDBvalue), for: UIControlEvents.touchUpInside)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +24,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func getDBvalue(){
+        guard let userText = userNameText.text else { return }
+        guard let passwordText = passwordText.text else {return}
+        
+        Database.database().reference().child("ShopManagement").child(userText).observeSingleEvent(of: DataEventType.value, with: { (Snapshot) in
+        
+            guard let dictionary = Snapshot.value as? [String:Any] else {return}
+            guard let userPassWordText = dictionary["PassWord"] as? String else {return}
+            
+            if userPassWordText == passwordText{
+                print("Login Succese")
+                self.performSegue(withIdentifier: "loginSuccess", sender: nil)
+            }else{
+                // MARK: -有小bug（第一次login fail不會print 要等login success後）
+                print("Login False")
+            }
+//            if userPassword == passwordText{
+//                print("Login Success")
+//            }else{
+//                print("Login False")
+//            }
+            
+        }) { (Error) in
+            print("error",Error)
+        }
+    }
 
 }
 
--- Admin/ViewControllerProflie.swift
+++ Admin/ViewControllerProflie.swift
@@ -11,25 +11,57 @@ import Firebase
 
 class ViewControllerProflie: UIViewController {
     
-   
+   var userName = [String]()
     @IBOutlet weak var profileName: UILabel!
     @IBOutlet weak var profilePic: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let profileID = UserDefaults.standard.object(forKey:"proflieID") as! NSArray
-        
-        let image = UIImage(data: profileID[0] as! Data)
-        profilePic.setImage(image, for: UIControlState.normal)
-        profilePic.layer.cornerRadius = profilePic.frame.width/2
-        profilePic.layer.masksToBounds = true
-        
-        profileName.text = profileID[2] as? String
+//        let profileID = UserDefaults.standard.object(forKey:"proflieID") as! NSArray
+//
+//        let image = UIImage(data: profileID[0] as! Data)
+//        profilePic.setImage(image, for: UIControlState.normal)
+//        profilePic.layer.cornerRadius = profilePic.frame.width/2
+//        profilePic.layer.masksToBounds = true
+//
+//        profileName.text = profileID[2] as? String
+        getUserInfo()
 
     }
     
     
     @IBAction func logOutButton(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    func getUserInfo(){
+        guard let userName = profileName.text else { return }
+//        Database.database().reference().child("ShopManagement").child(userName).observeSingleEvent(of: DataEventType.value) { (Snapshot) in
+//                      guard let dictionary = Snapshot.value as? [String:Any] else {return}
+//            profileName.text = Database.database().reference().child("ShopManagement").child(userName).
+//        }
+        Database.database().reference().child("ShopManagement").child("Shop1").observeSingleEvent(of: .value, with: { (snapshot) in
+            guard let dictionary = snapshot.value as? [String:Any] else { return }
+            guard let profileUserName = dictionary["userName"] as? String else { return }
+            self.profileName.text = profileUserName
+            print(snapshot)
+            //var foodNameArray = self.getUserName(snapshot)
+//            profileName.text = foodNameArray
+//            var foodPriceArray = self.getFoodPrice(snapshot)
+//            cell.lbName.text = foodNameArray[indexPath.row]
+//            cell.lbPrice.text = "Price:\(foodPriceArray[indexPath.row])"
+        }) { (error) in
+            print("Error get food detail:",error)
+        }
+//     profileName.text = Database.database().reference().child("ShopManagement").child(userName).
+
+    }
+    func getUserName(_ snapshot:DataSnapshot)->[String]{
+        for food in snapshot.children.allObjects as! [DataSnapshot]{
+           print("food.key")
+            //userName += [food.key]
+            
+        }
+        print(userName)
+        return userName
     }
     
     
